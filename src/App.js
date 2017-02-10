fbbde59968efed723db9fb4fdbd3f1612c0bcf02
Luke Ghenco <lukeghenco@gmail.com>
2017-02-10 14:42:58 -0500

    Update README.md with some small code fixes and bold test

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ class App extends Component {
   handleOnClickUsers() {
     this.props.store.dispatch({
       type: 'GET_COUNT_OF_USERS',
-    })
+    });
   }
 
   render() {
-    debugger;
+    // debugger;
     return (
       <div className="App">
           <button onClick={() => this.handleOnClickItems()}>
@@ -27,7 +27,7 @@ class App extends Component {
           <button onClick={() => this.handleOnClickUsers()}>
             Click to change user count
           </button>
-          <p> {this.props.items.length}</p>
+          <p>{this.props.items.length}</p>
       </div>
     );
   }
